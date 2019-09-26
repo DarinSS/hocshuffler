7be1a762f52903d2b06b2e1074ac0dfa66ad470b
DarinSS <kjsigig@naver.com>
2019-09-26 18:44:26 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -59,6 +59,11 @@ function select_cards(){
 	var shuffledcard = arrayShuffle(cardlist);
 	shuffledcard = shuffledcard.slice(0,10);
 	shuffledcard.sort(function(a,b){
+		if(a[0]>b[0]) return 1;
+		if(a[0]<b[0]) return -1;
+		return 0;
+	});
+	shuffledcard.sort(function(a,b){
 		if(a[1]>b[1]) return 1;
 		if(a[1]<b[1]) return -1;
 		return 0;
